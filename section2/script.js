9a8e9a6ca2fca54c1da55ed9196addd88c7b9247
Bird Xreal <saraichuchi@gmail.com>
2020-09-08 18:42:46 -0400

    Update script.js

--- section2/script.js
+++ section2/script.js
@@ -1,5 +1,5 @@
 /******************************** 
- Varaiables and data types
+//Varaiables and data types
  
  var firstName = "John";
  console.log(firstName);
@@ -26,17 +26,42 @@ var if = 23;
 
 
 
- Variable mutation and type coercion
+//Variable mutation and type coercion
 
 
-var 
+var firstName = 'John';
+var age = 28;
+
+//Type Coercio
+consol.log(firsName + ' ' + age)
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a' + age + ' year old' + job + ;. Is he married? ' + isMarried);
+
+//Variable mutation
+age = 'twenty eight';
+job = 'driver'
+
+alert(firstName + ' is a ' + age + ' year old ' + ' . Is he married?' + is Married);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName )
 
 
-  Bacsic operators 
+
+
+
+//Bacsic operators 
+
+
 var year, yearJohn, yearMark;
 now = 2020;
 ageJohn = 28;
 ageMark = 33;
+
 // Math operators
 var yearJohn = now - 28;
 var yearMark = now - 33;
@@ -47,9 +72,11 @@ console.log(now + 2);
 console.log(now * 2);
 console.log(now / 2);
 
+
 // Logical operators
 var johnOlder = ageJohn < ageMark;
 console.log(johnOlder);
+
 
 // typeof operator
 console.log(typeof johnOlder);
@@ -58,15 +85,22 @@ console.log(typeof "Mark is older than John");
 var x;
 console.log(typeof x); 
 
+
+
+
+
 // Operator precedene
+
 
 var now = 2018;
 var yearJohn = 1989;
 var fullAge = 18;
 
-var isFullAge = now - yearJohn >= fullAge;
+//Multiple operators
+var isFullAge = now - yearJohn >= fullAge; //true
 console.log(isFullAge);
 
+//Grouping
 var ageJohn = now - yearJohn;
 var ageMark = 35;
 var average = (ageJohn + ageMark) / 2;
@@ -77,16 +111,18 @@ var x, y;
 x = y = (3 + 5) * 4 - 6;
 console.log(x, y);
 
+
 // More operators
 x *= 2;
 console.log(x);
 x += 10;
 console.log(x);
-// all the same
 x = x + 1;
 x += 1;
 x--;
 console.log(x); 
+
+
 
 // challenge 1
 
@@ -105,7 +141,22 @@ console.log(bmiMark);
 var bmiMore = bmiJohn > bmiMark;
 console.log("Is John's BMI higher then Marks?  " + bmiMore);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //If / else statments
+
 
 var firstName = "John";
 var civilStatus = "single";
@@ -115,6 +166,7 @@ if (civilStatus === "married") {
 } else {
   console.log(firstName + " will hopefully marry soon:)");
 }
+
 
 var isMarried = true;
 if (isMarried) {
@@ -175,7 +227,8 @@ if (age >= 18) {
 } else {
   var drink = "juice";
 }
-// all mean the same ternanry ^
+ all mean the same ternanry ^
+
 //switch statments
 var job = "instructor";
 switch (job) {
@@ -312,8 +365,9 @@ function yearsUntilRetirement(year, firstName) {
 yearsUntilRetirement(1990, "John");
 yearsUntilRetirement(1948, "Mike");
 yearsUntilRetirement(1969, "Jane");
-*/
+
 //Function Statements and Expressions
+
 //function declaration
 // function whatDoYouDo(job,firstName){
 
@@ -331,3 +385,118 @@ switch (job) {
   default:
     return firstName + "does something else";
 }
+console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('designer', 'Jane'));
+console.log(whatDoYouDo('retired', 'Mark'));
+
+
+//Arrays
+
+//Initialize new array
+var names = ["John", "Mark", "Jane"];
+var years = new Array(1990, 1969, 1948);
+
+console.log(name[2]);
+console.log(names.length);
+
+//Mutate array data
+name[1] = "Ben";
+name[name.length] = "Mary";
+console.log(names);
+
+//Different data types
+var John = ["John", "Smith", 1990, "designer", false];
+
+John.push("blue");
+John.unshift("Mr.");
+console.log(john);
+
+console.log(john.indexOf(23));
+
+var isDesigner =
+  john.indexOf("designer") === -1
+    ? "John is NOT a designer"
+    : "John IS a designer";
+console.log(isDesigner);
+
+
+ Objects and properties
+
+// Object literal
+var john = {
+    firstName: 'John',  --key value pair
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false
+};
+console.log(john.firstName);
+console.log(john['lastName']);
+var x = 'birthYear';
+console.log(john[x]);
+john.job = 'designer';
+john['isMarried'] = true;
+console.log(john);
+
+// new Object syntax
+var jane = new Object();
+jane.firstName = 'Jane';
+jane.birthYear = 1969;
+jane['lastName'] = 'Smith';
+console.log(jane);
+
+
+  * Objects and methods
+    
+--this key word
+
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1992,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function(){
+        this.age = 2018  - this.birthYear;
+    }
+};
+john.calcAge();
+console.log(john);
+
+//Codeing Challenge 4
+
+var john = {
+  fulltName: "John Snow",
+  mass: 56,
+  height: 1.8,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+var mark = {
+  fullName: "Mark Pull",
+  mass: 60,
+  height: 1.4,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+john.calcBMI();
+mark.calcBMI();
+console.log(john, mark);
+
+if (john.bmi > mark.bmi) {
+  console.log(johm.fullName + " has higher BMI of" + john.bmi);
+} else if (mark.bmi > john.bmi) {
+  console.log(mark.fullName + "has a higher BMI of " + mark.bmi);
+} else {
+  console.log("They have the same BMI");
+}
+*/
+//Loops amd iteration
