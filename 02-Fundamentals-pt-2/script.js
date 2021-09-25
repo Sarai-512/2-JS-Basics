09cd68a111b87fef1c19747bd887427bf2bba11a
Sarai <66326163+Sarai-512@users.noreply.github.com>
2021-09-24 23:25:45 -0400

    commit

--- 02-Fundamentals-pt-2/script.js
+++ 02-Fundamentals-pt-2/script.js
@@ -103,7 +103,8 @@ const yearsUntilRetirement = function (birthYeah, firstName) {
 }
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1950, 'Mike'));
-/*
+
+
 ---Coding Challenge #1---
 Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
 Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
@@ -160,6 +161,7 @@ scoreKoalas = calcAverage(23, 34, 27);
 console.log(scoreDolphins, scoreKoalas);
 checkWinner(scoreDolphins, scoreKoalas);
 //array is a data structer arrays and  object are most important
+
 ---Introduction to Arrays--- 
 const friend1 = 'Sara';
 const friend2 = 'Larisa';
@@ -203,4 +205,111 @@ console.log(age1, age2, age3)
 const ages = [calcAge(years[0]),calcAge(years[1]), calcAge(years[years.length -1])];
 console.log(ages)
 
----Basic Array Operations(methods)--- */
+---Basic Array Operations(methods)---  
+const friends = ['Sara', 'Larisa', 'Bird']
+
+// Add elements
+const newLength = friends.push('wolf');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf('Larisa'));
+console.log(friends.indexOf('Bob'));
+friends.push(23);
+
+console.log(friends.includes('Larisa'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+if (friends.includes('Larisa')) {
+  console.log('You have a friend called Larisa');
+}
+
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+const bills = [125, 555, 44]
+
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+GOOD LUCK
+
+const calcTip = function (bill) {
+  return  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [125, 555, 44]
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
+
+---Intro to Objects---
+
+const birdsArray = ['Bird', 'Craven', 2037-1999,'dev', ['Larisa','sara','bird']];
+
+//boject with value pairs 
+//a name to each value
+//each key is called a properties this below has 5 properties
+//in arrays orders matter a lor
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven']
+};
+
+console.log(birdsArray, jonas)
+
+---How to get data from an Object Dot vs. Bracket Notation ---*/
+
+const bird = {
+  firstName: 'Bird',
+  lastName: 'Craven',
+  age: 2037-1999,
+  job: 'dev',
+  friends: ['Larisa','Sara','Me']
+};
+
+console.log(bird);
+//in square brackets we can put any expression
+console.log(bird.lastName);
+console.log(bird['lastName']);
+
+const nameKey = 'Name';
+
+console.log(bird['first'+ nameKey]); //makes firstName
+console.log(bird['last' + nameKey]); //makes lastName
+
+//console.log(bird.'last' +nameKey) this wont work
+
+const intersetedIn = prompt('What do you want to know about Bird? Choose between firstName, lastName, age, job, and friends');
+
+console.log(bird[intersetedIn]);
+
+if (bird [interestedIn]) {
+  console.log(bird[interestedIn]);
+} else {
+  console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+}
+bird.location = 'Portugal';
+bird['twitter'] = '@birdxreal';
+console.log(bird);
