4bed41e3f9ab638d5038a412d578cf02a93c2481
Sarai <66326163+Sarai-512@users.noreply.github.com>
2021-09-25 23:01:42 -0400

    update

--- 02-Fundamentals-pt-2/script.js
+++ 02-Fundamentals-pt-2/script.js
@@ -279,7 +279,7 @@ const jonas = {
 
 console.log(birdsArray, jonas)
 
----How to get data from an Object Dot vs. Bracket Notation ---*/
+---How to get data from an Object Dot vs. Bracket Notation ---
 
 const bird = {
   firstName: 'Bird',
@@ -303,13 +303,106 @@ console.log(bird['last' + nameKey]); //makes lastName
 
 const intersetedIn = prompt('What do you want to know about Bird? Choose between firstName, lastName, age, job, and friends');
 
-console.log(bird[intersetedIn]);
+console.log(bird[ intersetedIn]);
 
-if (bird [interestedIn]) {
-  console.log(bird[interestedIn]);
+if (bird [ intersetedIn]) {
+  console.log(bird[ intersetedIn]);
 } else {
   console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
 }
-bird.location = 'Portugal';
+bird.location = 'Japan';
 bird['twitter'] = '@birdxreal';
 console.log(bird);
+
+console.log(`${bird.firstName} has ${bird.friends.length} and her best friend is ${bird.friends[0]}`);
+
+---Objects Methoods--- 
+
+const bird = {
+  firstName: 'bird',
+  lastName: 'Craven',
+  birthYear: 1999,
+  job: 'Dev',
+  friends: ['Sara', 'Larisa', 'Tessa'],
+  hasDriversLicense: true,
+
+  //the this key word to use directly from the object so we dont repeat 
+  // calcAge: function (birthYear) {
+  //   return 2021 - birthYear;
+  // }
+
+  // calcAge: function () {
+  //   console.log(this);
+  //   return 2021 - this.birthYear;
+  // }
+
+  calcAge: function () {
+    this.age = 2021 - this.birthYear;
+    return this.age;
+  }
+
+} ;
+
+console.log(bird.calcAge());
+
+console.log(bird.age);
+console.log(bird.age);
+console.log(bird.age);
+
+// console.log(bird['calcAge'](1999));
+
+// console.log(`${bird.firstName} has ${bird.friends.length} and her best friend is ${bird.friends[0]}`);
+
+console.log(`${bird.firstName} is a ${bird.age} year old ${bird.job}`);
+
+console,log(bird,getSummary)
+
+//'Bird is a 46 year old teacher.'
+// Coding Challenge #3
+
+/*
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+
+3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+GOOD LUCK
+
+const mark = {
+  fullName: 'Mark Miller',
+  mass: 78,
+  height: 1.69, 
+
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  }
+};
+
+const john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95, 
+  
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  }
+
+};
+ mark.calcBMI();
+ john.calcBMI();
+
+console.log(mark.bmi, john.bmi);
+
+
+if (mark.bmi > john.bmi) {
+  console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`)
+} else if (john.bmi > mark.bmi) {
+  console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+}
+
+*/
