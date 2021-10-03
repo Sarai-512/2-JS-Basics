fa18293ad3c0ab61b960648f98c8b1002478b454
Sarai <66326163+Sarai-512@users.noreply.github.com>
2021-10-02 22:00:38 -0400

    commit

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -19,21 +19,39 @@ such as timers and fetch
 
 let secretNumber = Math.trunc(Math.random()*20) + 1 ; 
 let score = 20;
+let highscore = 0;
 console.log(secretNumber)
 
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
 
     console.log(typeof guess,guess);
-//one senerio where there is no number
+ //one senerio where there is no number
     if(!guess){
         document.querySelector(".message").textContent = "No number!";
     } else if (guess === secretNumber){
         document.querySelector(".message").textContent = "Correct Number!";
         document.querySelector('.number').textContent = secretNumber;
 
-        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('body').style.backgroundColor = 'rgb(13, 182, 117)';
         document.querySelector('.number').style.width = '30rem';
+
+        if(score > highscore){
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+
+    else if (guess !== secretNumber ) {
+        if(score > 1){
+            document.querySelector('.message').textContent = guess > secretNumber ? "To High!" : "To low!" ;
+        score = score - 1; 
+        document.querySelector('.score').textContent = score;
+        }else {
+            document.querySelector('.message').textContent = "You lost the game!";
+            document.querySelector('.score').textContent = 0;
+        }
+    }    
+    //could now delete all of this becasue of conditinal turnert=y operator 
 
     }else if (guess > secretNumber){
         if(score > 1){
@@ -95,8 +113,15 @@ document.querySelector('.again').addEventListener('click', function(){
     secretNumber = Math.trunc(Math.random()*20) + 1 ; 
 
     document.querySelector(".message").textContent = "Start guessing...";
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = "?";
+    document.querySelector('.guess').value = " ";
+
+    document.querySelector('.number').style.width = '15rem';
+    
+    document.querySelector('body').style.backgroundColor = '#222';
    
-};
+});
 
 
 
---
+++ 06-Modal/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsShowModal);
+
+for (let i = 0; i < btnsShowModal.length; i++)
+
+    btnsShowModal[i].addEventListener('click', function(){
+
+        console.log('Button clicked');
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
