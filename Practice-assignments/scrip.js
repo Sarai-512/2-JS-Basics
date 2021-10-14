cb88e7948dd73640f7753abcfb5e2d3be1650c4c
Sarai <66326163+Sarai-512@users.noreply.github.com>
2021-10-13 21:24:27 -0400

    script.js

--- Practice-assignments/scrip.js
+++ Practice-assignments/scrip.js
@@ -167,4 +167,13 @@ when you input 1
 situation
 */
 
+const numNeighbours = Number(prompt('How many neighbour countries does your countryhave?'));
+console.log(numNeighbours);
 
+if (numNeighbours == 1) { 
+    console.log('Only 1 border!'); 
+} else  (numNeighbours ===1 ) { 
+    console.log('More than 1 border');
+} else () {
+
+}
