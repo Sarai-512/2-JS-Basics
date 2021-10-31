ce0b3eecf99febafedd964e216b02d75a3f9d3a7
Sarai <66326163+Sarai-512@users.noreply.github.com>
2021-10-31 14:02:05 -0400

    commit

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -55,6 +55,56 @@ const newPassport = function (person) {
 newPassport(jonas);
 checkIn(flight, jonas);
 
-  */
+  
 
-//Functions Accepting Callback Functions
+//[-------Functions Accepting Callback Functions------]
+//functions that accept functions as input
+
+const oneWord = function(str){
+  return str.replace(/ /g, '').toLowerCase();
+}
+
+const upperFirstWrod = function(str){
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join( ' '); 
+};
+
+//higher-order function
+const transformer = function(str, fn) {
+  console.log(`Original string: ${str}`)
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformedby: ${fn.name}`)
+}
+
+transformer('Javascript is the best!', upperFirstWrod);
+
+transformer('Javascript is the best!', oneWord);
+
+//JS uses callbacks all the time
+const high5 = function () {
+  console.log('emjoi');
+};
+
+document.body.addEventListener('click', emjoi);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+//[-------Functions Returning Functions-----------]
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+//greeterHey becomes a function
+const greeterHey = greet('Hey');
+greeterHey('Jonas');   //Hey Jonas
+greeterHey('Steven');  //Hey Steven
+
+greet('Hello')('Jonas');
+
+// Challenge make the function into arrow function
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+greetArr('Hi')('Jonas');
+
+*/
