c7fd46bf8ab414450c131fefe97ac34df40238e2
Sarai <66326163+Sarai-512@users.noreply.github.com>
2021-11-29 14:31:12 -0500

    commit

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -325,7 +325,7 @@ booker();
 booker();
 
 console.dir(booker);
- */
+ 
 // ------More Closure Examples------
 // -----Example 1----
 let f; //empty variable , defined outside global scope
@@ -364,6 +364,30 @@ const boardPassengers = function (n, wait) {
   }, wait * 1000);
   console.log(`Will start boarding in ${wait} seconds`);
 };
+
 const perGroup = 1000;
 boardPassengers(180, 3);
 
+// Coding Challenge #2
+
+
+This is more of a thinking challenge than a coding challenge 🤓
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+GOOD LUCK 😀
+
+
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click',function () {
+    header.style.color = 'orange';
+  });
+
+})();
+
+//how does this callback funtion get access to the header varible explanation is the closer
+
+*/
